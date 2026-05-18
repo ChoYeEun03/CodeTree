d400473cc16dd2428372ddcbb425c0725f623b65
ChoYeEun03 <132871279+ChoYeEun03@users.noreply.github.com>
2026-05-18 12:29:17 +0900

    [Passed] 건너뛰고 별표 출력하기 (49ms, 16MB)

---
+++ Python/trail1/건너뛰고 별표 출력하기/skip-and-print-a-star.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range(n-1, -1, -1):
+    for j in range(n-i):
+        print("*", end = "")
+    print()
+    print()
+
+for i in range(n-1, 0, -1):
+    for j in range(i):
+        print("*", end = "")
+    print()
+    print()
