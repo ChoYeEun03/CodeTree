be0684599eb166979e838027d0a60d54f7a81f24
ChoYeEun03 <132871279+ChoYeEun03@users.noreply.github.com>
2026-05-29 11:02:26 +0900

    [Passed] 완전수 (59ms, 17MB)

---
+++ Python/trail1/완전수/perfect-number.py
@@ -0,0 +1,11 @@
+start, end = map(int, input().split())
+cnt=0
+for i in range(start, end):
+    sum=0
+    for j in range(1, i):
+        if i % j ==0:
+            sum+=j
+    if sum == i:
+        cnt+=1
+print(cnt)
+
