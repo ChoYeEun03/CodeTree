3fdc5eededa60f9bb80f96eb12ba7b0a318f4525
ChoYeEun03 <132871279+ChoYeEun03@users.noreply.github.com>
2026-05-29 11:13:53 +0900

    [Passed] 소수 구하기 (57ms, 16MB)

--- Python/trail1/소수 구하기/get-prime.py
+++ Python/trail1/소수 구하기/get-prime.py
@@ -7,3 +7,20 @@ for i in range(1,n+1):
             cnt +=1
     if cnt ==2:
         print(i, end = " ")
+
+# 아래는 정답 코드인데 for i in range(2,2)면 그냥 넘어가서 true를 유지한다는 게 신기해서 남김
+# 2도 소수로 출력됨
+#n = int(input())
+    
+# 1부터 n까지 소수를 구합니다.
+# for i in range(1, n + 1):
+#     if i == 1:
+#         continue
+#     isprime = True
+    
+#     for j in range(2, i):
+#         if i % j == 0:
+#             isprime = False
+
+#     if isprime:
+#         print(i, end=" ")
