63b4df3e05c62539cd96489bdcc29f9d2f6efe61
ChoYeEun03 <132871279+ChoYeEun03@users.noreply.github.com>
2026-07-02 14:44:42 +0900

    [Passed] Date to Date (47ms, 16MB)

---
+++ Python/trail2/Date to Date/date-to-date.py
@@ -0,0 +1,31 @@
+m1, d1, m2, d2 = map(int, input().split())
+month =[0,31,28,31,30,31,30,31,31,30,31,30,31]
+day = 1
+
+
+while True:
+    if m1==m2 and d1 == d2:
+        break
+    day +=1
+    d1+=1
+    if d1>month[m1]:
+        m1 +=1
+        d1 =1
+print(day)
+
+
+
+
+
+
+        
+    
+
+
+
+
+
+
+
+
+# Please write your code here.
