035281a3dafa61e98ec8354472e9a93dcd7781ea
ChoYeEun03 <132871279+ChoYeEun03@users.noreply.github.com>
2026-07-09 23:38:26 +0900

    [Passed] 코로나 메뉴얼 (50ms, 16MB)

--- Python/trail1/코로나 메뉴얼/covid-manual.py
+++ Python/trail1/코로나 메뉴얼/covid-manual.py
@@ -1,11 +1,19 @@
 cnt=0
+dict={"A":0, "B":0, "C":0, "D": 0}
 for i in range(3):
     a,b = input().split(" ")
-    if a =='Y':
-        if int(b) >=37:
-            cnt +=1
+    if a =='Y' and int(b)>=37:
+        flag ="A"
+    elif a=='N' and int(b)>=37:
+        flag = "B"
+    elif a=="Y" and int(b)<37:
+        flag = "C"
+    else:
+        flag = "D"
+    dict[flag] +=1
 
-if cnt >=2:
+
+if dict["A"] >=2:
     print("E")
 else:
     print("N")
