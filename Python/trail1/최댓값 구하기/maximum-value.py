53ef23c69e48e75fddd7a86976e1440195e63c02
ChoYeEun03 <132871279+ChoYeEun03@users.noreply.github.com>
2026-07-09 23:34:29 +0900

    [Passed] 최댓값 구하기 (53ms, 16MB)

--- Python/trail1/최댓값 구하기/maximum-value.py
+++ Python/trail1/최댓값 구하기/maximum-value.py
@@ -1,11 +1,13 @@
 a,b,c = map(int,input().split())
-max = a
 
-if b> max:
-    if c>b:
-        max =c
-    else:
-        max = b
-elif c>max:
-    max = c
+max=a
+if b>= a and b>=c:
+    max =b
+if c>=b and c>=a:
+    max =c
+if a>=b and a>=c:
+    max =a
+
+
+
 print(max)
