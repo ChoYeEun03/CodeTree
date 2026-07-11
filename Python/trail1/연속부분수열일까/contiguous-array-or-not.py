0741d81c6abf9b355933c03c27cdf477ecbeaf4f
ChoYeEun03 <132871279+ChoYeEun03@users.noreply.github.com>
2026-07-11 15:04:17 +0900

    [Passed] 연속부분수열일까 (64ms, 16MB)

--- Python/trail1/연속부분수열일까/contiguous-array-or-not.py
+++ Python/trail1/연속부분수열일까/contiguous-array-or-not.py
@@ -1,21 +1,22 @@
 n, m = map(int, input().split(" "))
 arr1= list(map(int, input().split(" ")))
 arr2= list(map(int, input().split(" ")))
-arr3=[]
+
 cnt =0
 def find(k):
-    global m
     for i in range(m):
         if k >=n:
             return 0
-        if arr1[k]==arr2[i]:
-            arr3.append(0)
-            if k>n:
-                return 0
-            k+=1
-            continue
-        else:
+        # if arr1[k]==arr2[i]:
+        #     if k>n:
+        #         return 0
+        #     k+=1
+        #     continue
+        # else:
+        #     return 0
+        if arr1[k]!=arr2[i]:
             return 0
+        k+=1
     return 1
 for i in range(n):
     if arr1[i] == arr2[0]:
